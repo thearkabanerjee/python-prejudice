e37cfea0295ea5a3e4fbb7bfd4bec4f7ddb46d2e
Arka Banerjee <arkabanerjee381@gmail.com>
2025-09-30 18:03:18 +0530

    assignment week 1

--- w1a1.py
+++ w1a1.py
@@ -6,3 +6,33 @@ print (type(c)) # <class 'int'>
 
 print (type (a/b)) # <class 'float'>
 
+print (type (a**b))
+
+# what will be the output of the following expression => '5' * 2
+m = ('5' * 2 )
+print (type (m))
+
+# What will be the output type of the expression '5' + "2"?
+n = ('5' + "2")
+print (type (n))
+
+# What will be the output type of the expression 5 == 2?
+o = (5 == 2)
+print (type (o))
+
+# What will be the output type of the expression 'True' or 'False'?
+p = ('True' or 'False')
+print (type (p))
+
+#what will be the output type of the following expression 5 == 5 and "2"
+q = (5 == 5 and "2")
+print (type (q))
+
+#What will be the output type of the expression "555["2"]"?
+# r = ("555["2"]")
+# print (type (r))
+
+# What will be the output type of the expression "555""[2]" ?
+s = ("555""[2]")
+print (type (s))
+
