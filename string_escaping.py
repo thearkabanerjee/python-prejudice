02b4a8dc4e97bec5db0c021c67e0f0cbdf525e70
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-04 15:39:09 +0530

    practicing_python

---
+++ string_escaping.py
@@ -0,0 +1,17 @@
+# A single quote ' and a double quote "
+output1 = """A single quote ' and a double quote \""""  # already given
+
+# A forward slash / and a backward slash \
+output2 = """A forward slash / and a backward slash \\"""
+
+# Three single quotes ''' and three double quotes """
+output3 = """Three single quotes ''' and three double quotes \"\"\""""
+
+# Double forward slash // and Double backward slash \\
+output4 = """Double forward slash // and Double backward slash \\\\"""
+
+
+print (output1)
+print (output2)
+print (output3) 
+print (output4)
