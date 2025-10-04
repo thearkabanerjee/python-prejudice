d34b144cb99d16b9d27eb37947749e7627d1d258
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-04 17:48:51 +0530

    practicing_python

---
+++ another_one.py
@@ -0,0 +1,20 @@
+# Write a program to calculate the cost of a meal at a restaurant.
+
+# The cost of the meal depends on the main dish and the time of the day.
+# Main dishes available:
+# "paneer tikka": ₹250
+# "butter chicken": ₹240
+# "masala dosa": ₹200
+# Additional discounts:
+# 15% discount for meals ordered during lunchtime (from 12 PM to 3 PM).
+# Customers can apply vouchers for an additional 10% discount on the total cost.
+# The restaurant accepts card payments.
+# For card payments, there's an additional service charge of 5% on the total cost after applying any discounts.
+# Take the following inputs:
+# Main dish : str
+# Time of the day: int: 24-hour format
+# Whether the customer has a voucher: bool: True/False
+# Whether the payment is by card: bool: True/False
+# Calculate and display the total cost of the meal.
+
+# need to do
