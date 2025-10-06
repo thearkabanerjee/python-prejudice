a67907451c0fbd5b6f0301acea45dd544c60a979
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-06 10:34:01 +0530

    python

--- randint.py
+++ randint.py
@@ -3,7 +3,13 @@
 import numpy as np
 
 # first we are importing the numpy library
-a = np.random.randint(1,10)
-print(a)
+a = np.random.randint(1,6)
+# what the function does is generating a random number between 1 and 6
 
+b = np.random.randint(1,7, 10)
+# generates a random number between 1 and 6 again but this time, we are asking for 10 guesses 
+
+print("the die roll guess is ", a)
+
+print ("the probable die roll guesses are",b)
 
