1606112c9be680979d93e26d9b9bdd1187102f64
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-15 20:49:09 +0530

    python

--- WEEK-3/graded-assignment.py
+++ WEEK-3/graded-assignment.py
@@ -9,3 +9,7 @@ print('|')
 
 # if the while loop of the below code executes, what does it terminates
 
+x = int(input("Enter any number: "))
+while(x % 5 != 0 and x % 10 != 0):
+    x = int(input("Enter any number: "))
+print("outside loop, the value of x is ", x)
