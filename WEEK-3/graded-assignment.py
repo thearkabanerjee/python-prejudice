ba275dde11e74cd6d149a6987ded31b47dbf9020
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-15 20:44:35 +0530

    python

--- WEEK-3/graded-assignment.py
+++ WEEK-3/graded-assignment.py
@@ -1,2 +1,8 @@
 # what is the output of the following code
 
+for char in 'a1b2c3d4e5':
+    if char in 'abcde':
+        print('|', end = '') # there is no space between the quotes
+        continue
+    print(char, end = '')  # there is no space between the quotes
+print('|')
