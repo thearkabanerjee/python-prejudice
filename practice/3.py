c3d851e775e0e78023f14126c529558e38ee8b72
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-17 13:39:31 +0530

    python

---
+++ practice/3.py
@@ -0,0 +1,11 @@
+
+
+# what is the output of the following code
+
+# enter an integer as your answer
+
+lst = ['IIT', ['Madras', '2025'], 'exam']
+
+print(len(lst[0]) + len(lst[1][1]) + len(lst[2]))
+
+# 3 + 4 + 4
