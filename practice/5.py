72450ea1afc2f90819deb732d26f6d41fdc355bc
Arka Banerjee <arkabanerjee381@gmail.com>
2025-10-19 15:44:56 +0530

    python

--- practice/5.py
+++ practice/5.py
@@ -8,6 +8,11 @@ name = "arka"
 
 # expected printing of bslb
 
-for char in name:
-    shifted = ''.join(chr(ord(char) + 1) for char in name)
-print (shifted)
+# expected code might be 
+
+name = "arka"
+shifted = ""
+for c in name:
+    shifted += chr(ord(c)+1)
+print(shifted)  # Output: bslb
+
